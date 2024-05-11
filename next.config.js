2eae7c33a3037039c91b1d9c9ad60ded61ff2f61
Kenny <pelusoken@gmail.com>
2024-05-11 18:05:03 -0400

    structural overhaul; make images local; add script to fix links; add contributions; revamp welcome

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.tsx'
+  themeConfig: './theme.config.tsx',
 })
- 
+
 module.exports = withNextra()
- 
+
 // If you have other Next.js configurations, you can pass them as the parameter:
 // module.exports = withNextra({ /* other next.js config */ })
 // Learn more: https://nextra.site/docs/guide
---
+++ scripts/fixRelativeLinks.js
@@ -0,0 +1,79 @@
+const fs = require('fs');
+const path = require('path');
+const util = require('util');
+
+const readdir = util.promisify(fs.readdir);
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+const stat = util.promisify(fs.stat);
+
+const directoryPath = '/path/to/your/docs';
+
+async function getAllFiles(dirPath, arrayOfFiles) {
+  const files = await readdir(dirPath);
+
+  arrayOfFiles = arrayOfFiles || [];
+
+  await Promise.all(files.map(async file => {
+    if (fs.statSync(path.join(dirPath, file)).isDirectory()) {
+      arrayOfFiles = await getAllFiles(path.join(dirPath, file), arrayOfFiles);
+    } else {
+      arrayOfFiles.push(path.join(dirPath, file));
+    }
+  }));
+
+  return arrayOfFiles;
+}
+
+async function findAndUpdateLinks(files) {
+  let fileMap = {};
+
+  files.forEach(file => {
+    const baseName = path.basename(file);
+    if (!fileMap[baseName]) {
+      fileMap[baseName] = [];
+    }
+    fileMap[baseName].push(file);
+  });
+
+  for (const [filename, paths] of Object.entries(fileMap)) {
+    if (paths.length > 1) {
+      console.log(`Duplicate filename detected: ${filename}. Files: ${paths.join(', ')}. Please rename to make them unique.`);
+    }
+  }
+
+  await Promise.all(files.map(async file => {
+    const content = await readFile(file, 'utf8');
+    const dir = path.dirname(file);
+    let modified = content;
+
+    const linkRegex = /\]\((?!http)(.+?)\)/g;
+    let match;
+
+    while ((match = linkRegex.exec(content)) !== null) {
+      const [fullMatch, linkPath] = match;
+      const targetFile = path.resolve(dir, linkPath);
+
+      if (!fs.existsSync(targetFile)) {
+        console.log(`Broken link found in ${file}: ${linkPath} does not exist.`);
+      } else {
+        const correctPath = path.relative(dir, fileMap[path.basename(targetFile)][0]);
+        modified = modified.replace(fullMatch, `](${correctPath})`);
+      }
+    }
+
+    if (modified !== content) {
+      await writeFile(file, modified, 'utf8');
+      console.log(`Updated links in ${file}`);
+    }
+  }));
+}
+
+(async () => {
+  try {
+    const files = await getAllFiles(directoryPath);
+    await findAndUpdateLinks(files);
+  } catch (error) {
+    console.error('An error occurred:', error);
+  }
+})();
